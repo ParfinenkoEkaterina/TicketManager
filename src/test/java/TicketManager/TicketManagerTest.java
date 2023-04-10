f2cf3a113af3532810b22af6811043936c28765b
Ekaterina Parfinenko <katyavasilenko2012@gmail.com>
2023-04-10 13:02:50 +0300

    TicketByDurationAscComparator added

--- src/test/java/TicketManager/TicketManagerTest.java
+++ src/test/java/TicketManager/TicketManagerTest.java
@@ -124,4 +124,63 @@ public class TicketManagerTest {
     }
 
 
+    @Test
+    public void testFindsSortOfDuration() {
+        TicketRepository repo = new TicketRepository();
+        TicketManager manager = new TicketManager(repo);
+
+        Ticket ticket1 = new Ticket(1, "Moskow", "Paris", 500, 188);
+        Ticket ticket2 = new Ticket(2, "Moskow", "Paris", 550, 190);
+        Ticket ticket3 = new Ticket(3, "Moskow", "Paris", 200, 189);
+        Ticket ticket4 = new Ticket(4, "Minsk", "Sochi", 220, 160);
+        Ticket ticket5 = new Ticket(5, "Moskow", "Paris", 400, 192);
+        Ticket ticket6 = new Ticket(6, "Moskow", "Paris", 440, 191);
+        Ticket ticket7 = new Ticket(7, "SPB", "Murmansk", 200, 56);
+        Ticket ticket8 = new Ticket(8, "Murmansk", "SPB", 202, 58);
+
+        manager.add(ticket1);
+        manager.add(ticket2);
+        manager.add(ticket3);
+        manager.add(ticket4);
+        manager.add(ticket5);
+        manager.add(ticket6);
+        manager.add(ticket7);
+        manager.add(ticket8);
+
+        Ticket[] expected = {ticket1, ticket3, ticket2, ticket6, ticket5};
+        Ticket[] actual = manager.findAll("Moskow", "Paris", new TicketByDurationAscComparator());
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testNotFindsTicket() {
+        TicketRepository repo = new TicketRepository();
+        TicketManager manager = new TicketManager(repo);
+
+        Ticket ticket1 = new Ticket(1, "Moskow", "Paris", 500, 188);
+        Ticket ticket2 = new Ticket(2, "Moskow", "Paris", 550, 190);
+        Ticket ticket3 = new Ticket(3, "Moskow", "Paris", 200, 189);
+        Ticket ticket4 = new Ticket(4, "Minsk", "Sochi", 220, 160);
+        Ticket ticket5 = new Ticket(5, "Moskow", "Paris", 400, 192);
+        Ticket ticket6 = new Ticket(6, "Moskow", "Paris", 440, 191);
+        Ticket ticket7 = new Ticket(7, "SPB", "Murmansk", 200, 56);
+        Ticket ticket8 = new Ticket(8, "Murmansk", "SPB", 202, 58);
+
+        manager.add(ticket1);
+        manager.add(ticket2);
+        manager.add(ticket3);
+        manager.add(ticket4);
+        manager.add(ticket5);
+        manager.add(ticket6);
+        manager.add(ticket7);
+        manager.add(ticket8);
+
+        Ticket[] expected = {};
+        Ticket[] actual = manager.findAll("Paris", "Moskow", new TicketByDurationAscComparator());
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+
 }
